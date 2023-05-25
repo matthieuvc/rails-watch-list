67c76dc74323a97d4899ab4ea0a914317d5f566b
matthieu van cauwenbergh <vancauwenbergh.matthieu@gmail.com>
2023-05-25 15:46:35 +0200

    solution

--- Gemfile
+++ Gemfile
@@ -66,3 +66,4 @@ end
 
 gem 'rspec-rails', group: [ :test ]
 gem 'rails-controller-testing', group: [ :test ]
+gem "simple_form"
--- app/models/list.rb
+++ app/models/list.rb
@@ -1,5 +1,6 @@
 class List < ApplicationRecord
   has_many :bookmarks, dependent: :destroy
   has_many :movies, through: :bookmarks
-  validates :name, uniqueness: true, presence: true
+  validates :name, uniqueness: true
+  validates :name, presence: true
 end
