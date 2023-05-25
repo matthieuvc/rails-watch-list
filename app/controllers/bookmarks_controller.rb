25900d1490398a311a96339522352685db27a2ba
matthieu van cauwenbergh <vancauwenbergh.matthieu@gmail.com>
2023-05-25 16:53:13 +0200

    solution

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,30 @@
+class BookmarksController < ApplicationController
+  before_action :set_list, only: %i[new create]
+
+  def new
+    @bookmark = Bookmark.new
+    @bookmark.list = @list
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+    @movie = Movie.find(params[:bookmark][:movie_id])
+    @bookmark.movie = @movie
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment)
+  end
+
+  def set_list
+    @list = List.find(params[:list_id])
+  end
+end
