1605f87bd92a9aa26df97b41dc8a720adc36acdd
matthieu van cauwenbergh <vancauwenbergh.matthieu@gmail.com>
2023-05-26 11:59:06 +0200

    bundle update

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -18,6 +18,11 @@ class BookmarksController < ApplicationController
     end
   end
 
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+  end
+
   private
 
   def bookmark_params
